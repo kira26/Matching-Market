f4d9b66122148e1be9ed2d87e6615a2920e52717
小川慶将 <yshms_o@yahoo.co.jp>
2015-06-23 19:58:17 +0900

    Compute Many-to-One Matching

--- matching.py
+++ matching.py
@@ -1,119 +1,146 @@
 """
 Filename: matching.py
 Authors: Yoshimasa Ogawa
-LastModified: 22/06/2015
+LastModified: 23/06/2015
 
 A collection of functions to solve the matching problems.
 """
-
 import networkx
 import matplotlib.pyplot as plt
 import numpy as np
 import random
 
 
-def acceptable(m_id, f_id, m_num, f_prefs):
-    """
-    Check the partner is acceptable.
-    Returns True or False.
-    """
-    if (m_num in f_prefs[f_id]):
-        if f_prefs[f_id].index(m_id) < f_prefs[f_id].index(m_num):
-            return True
-        else:
-            return False
-    else:
-        return True
-
-
-def compare(m_id, f_id, f_prefs, f_matched):
-    """
-    Check the partner is better than ever.
-    Returns True or False.
-    """
-    if f_prefs[f_id].index(m_id) < f_prefs[f_id].index(f_matched[f_id]):
-        return True
-    else:
-        return False
-
-
-def make_prefs(m_num, f_num, allow_unmatched=True):
-    if allow_unmatched == True:
-        m_prefs = [range(0, f_num+1) for col in range(m_num)]
-        f_prefs = [range(0, m_num+1) for col in range(f_num)]
-    else:
-        m_prefs = [range(0, f_num) for col in range(m_num)]
-        f_prefs = [range(0, m_num) for col in range(f_num)]
-    for i in range(m_num):
-        random.shuffle(m_prefs[i])
-    for i in range(f_num):
-        random.shuffle(f_prefs[i])
-    return m_prefs, f_prefs
-
-
-def deferred_acceptance(m_prefs, f_prefs):
+def deferred_acceptance(prop_prefs, resp_prefs, caps=None):
     """
     For test_matching.py.
     Return 2 lists that include male, female pair information.
     """
     test = Marriage()
-    m_matched, f_matched = test.match(m_prefs, f_prefs)
-    return m_matched, f_matched
+    if caps == 0:
+        prop_matched, resp_matched = test.match(prop_prefs, resp_prefs, caps)
+        return prop_matched, resp_matched
+    else:
+        prop_matched, resp_matched, indptr = test.match(prop_prefs, resp_prefs, caps)
+        return prop_matched, resp_matched, indptr
 
 
 class Marriage:
-    """
-    Solve one-to-one matching problems by Deferred Acceptance Algorithm.
-    match() returns 2 lists, m_matched and f_matched.
-    graph() print a graph about the matching pair made by match().
-    """
-    def match(self, m_prefs, f_prefs):
-        self.m_prefs = m_prefs
-        self.f_prefs = f_prefs
-        self.m_num = len(m_prefs)
-        self.f_num = len(f_prefs)
-        self.m_matched = [None for col in range(self.m_num)]
-        self.f_matched = [None for col in range(self.f_num)]
-        self.m_name = range(self.m_num)
-        self.f_name = range(self.f_num)
-        m_single = range(self.m_num)
-        f_single = range(self.f_num)
-        while len(m_single) >= 1:
-            for i in m_single:
-                m_id = i
-                for j in self.m_prefs[i]:
-                    f_id = j
-                    if f_id == self.f_num:
-                        m_single.remove(i)
-                        self.m_matched[m_id] = self.f_num
+    def match(self, prop_prefs, resp_prefs, caps=None):
+        """
+        Compute matching problems by Deferred Acceptance Algorithm.
+
+
+        Parameters
+        ----------
+        prop_prefs : ndarray(int, ndim=2)
+            Preference of proposers
+
+        resp_prefs : ndarray(int, ndim=2)
+            Preference of respondants
+
+        Return
+        ------
+        if switch is 0 that means many-to-one matching,
+
+            prop_matches : ndarray(int, ndim=1)
+                Stable Matching Pairs in which index is
+                the number of proposers
+
+            resp_matched : ndarray(int, ndim=1)
+                Stable Matching Pairs
+
+            indptr : ndarray(int, ndim=1)
+                Array that shows which respondants match
+                with proposers in resp_matched
+
+
+        if switch is 1 that means one-to-one matching,
+
+            prop_matches : ndarray(int, ndim=1)
+                Stable Matching Pairs in which index is
+                the number of proposers
+
+            resp_matched : ndarray(int, ndim=1)
+                Stable Matching Pairs in which index is
+                the number of respondants
+
+        """
+        # Set the variables
+        self.prop_prefs = np.asarray(prop_prefs)
+        self.resp_prefs = np.asarray(resp_prefs)
+        self.prop_num = self.prop_prefs.shape[0]
+        self.resp_num = self.resp_prefs.shape[0]
+        prop_unmatched = self.resp_num
+        resp_unmatched = self.prop_num
+        if self.resp_prefs.shape[1] == self.prop_num:
+            self.resp_ranks = np.hstack((np.argsort(resp_prefs), np.tile(resp_unmatched, self.resp_num).reshape(self.resp_num, 1)))
+        else:
+            self.resp_ranks = np.argsort(resp_prefs)
+        switch = 0
+        if caps is None:
+            switch = 1
+            caps = [1 for col in range(self.resp_num)]
+        self.prop_matched = np.zeros(self.prop_num, dtype=int) + prop_unmatched
+        self.resp_matched = np.zeros([self.resp_num, max(caps)], dtype=int) + resp_unmatched
+        self.prop_name = range(self.prop_num)
+        self.resp_name = range(self.resp_num)
+        self.caps = caps
+        self.indptr = np.zeros(self.resp_num+1, dtype=int)
+        np.cumsum(caps, out=self.indptr[1:])
+
+        prop_single = range(self.prop_num)
+        caps_rest = [i for i in caps]
+
+        # Start matching
+        while len(prop_single) >= 1:
+            for i in prop_single:
+                prop_id = i
+                for j in self.prop_prefs[prop_id]:
+                    resp_id = j
+                    if resp_id == prop_unmatched:
+                        prop_single.remove(prop_id)
+                        self.prop_matched[prop_id] = prop_unmatched
                         break
-                    if f_id in f_single:
-                        if acceptable(m_id, f_id, self.m_num, self.f_prefs):
-                            m_single.remove(m_id)
-                            f_single.remove(f_id)
-                            self.m_matched[m_id] = f_id
-                            self.f_matched[f_id] = m_id
+                    elif caps_rest[resp_id] >= 1:
+                        if self.resp_ranks[resp_id][prop_id] < self.resp_ranks[resp_id][resp_unmatched]:
+                            prop_single.remove(prop_id)
+                            self.prop_matched[prop_id] = resp_id
+                            caps_rest[resp_id] -= 1
+                            self.resp_matched[resp_id][caps_rest[resp_id]] = prop_id
                             break
                     else:
-                        if compare(m_id, f_id, self.f_prefs, self.f_matched):
-                            m_single.append(self.f_matched[f_id])
-                            self.m_matched[self.f_matched[f_id]] = -1
-                            m_single.remove(i)
-                            self.m_matched[m_id] = f_id
-                            self.f_matched[f_id] = m_id
+                        deffered = self.resp_matched[resp_id][:self.caps[resp_id]]
+                        max_rank = max([self.resp_ranks[resp_id][i] for i in deffered])
+                        max_id = self.resp_prefs[resp_id][max_rank]
+                        if self.resp_ranks[resp_id][prop_id] < max_rank:
+                            prop_single.append(max_id)
+                            self.prop_matched[max_id] = prop_unmatched
+                            prop_single.remove(resp_id)
+                            self.prop_matched[prop_id] = resp_id
+                            deffered = np.hstack((np.delete(deffered, np.where(deffered == max_id)[0]), prop_id))
+                            self.resp_matched[resp_id] = deffered
                             break
-                if m_id in m_single:
-                    m_single.remove(i)
-                    self.m_matched[m_id] = self.f_num
-        for k in range(self.f_num):
-            if k in f_single:
-                f_single.remove(k)
-                self.f_matched[k] = self.m_num
-        return self.m_matched, self.f_matched
+                if prop_id in prop_single:
+                    prop_single.remove(i)
+                    self.prop_matched[prop_id] = prop_unmatched
+        resp_matched_comp0 = self.resp_matched
+        resp_matched_comp = np.zeros(sum(self.caps), dtype=int)
+        for i in range(self.resp_num):
+            resp_matched_comp[self.indptr[i]:self.indptr[i]+self.caps[i]] = self.resp_matched[i][:self.caps[i]]
+        self.resp_matched = resp_matched_comp
 
-    def set_name(self, m_name, f_name):
-        self.m_name = m_name
-        self.f_name = f_name
+        # Return the answer
+        if switch == 0:
+            return self.prop_matched, self.resp_matched, self.indptr
+        elif switch == 1:
+            return self.prop_matched, self.resp_matched
+        else:
+            return self.prop_matched, resp_matched_comp0
+
+    def set_name(self, prop_name, resp_name):
+        self.prop_name = prop_name
+        self.resp_name = resp_name
 
     def graph(self):
         """
@@ -126,21 +153,21 @@ class Marriage:
         pos = {}
         m_pos = {}
         f_pos = {}
-        height = max(self.m_num, self.f_num)
-        for i in range(self.m_num):
+        height = max(self.prop_num, self.resp_num)
+        for i in range(self.prop_num):
             vector["m%s" % i] = []
-            m_vector[self.m_name[i]] = []
+            m_vector[self.prop_name[i]] = []
             pos["m%s" % i] = np.array([1, height-i])
-            m_pos[self.m_name[i]] = np.array([1, height-i])
-            if self.m_matched[i] != self.f_num:
-                vector["m%s" % i].append("f%s" % self.m_matched[i])
-        for i in range(self.f_num):
+            m_pos[self.prop_name[i]] = np.array([1, height-i])
+            if self.prop_matched[i] != self.resp_num:
+                vector["m%s" % i].append("f%s" % self.prop_matched[i])
+        for i in range(self.resp_num):
             vector["f%s" % i] = []
-            f_vector[self.f_name[i]] = []
+            f_vector[self.resp_name[i]] = []
             pos["f%s" % i] = np.array([2, height-i])
-            f_pos[self.f_name[i]] = np.array([2, height-i])
-            if self.f_matched[i] != self.m_num:
-                vector["f%s" % i].append("m%s" % self.f_matched[i])
+            f_pos[self.resp_name[i]] = np.array([2, height-i])
+            if self.resp_matched[i] != self.prop_num:
+                vector["f%s" % i].append("m%s" % self.resp_matched[i])
         graph = networkx.Graph(vector)
         m_graph = networkx.Graph(m_vector)
         f_graph = networkx.Graph(f_vector)
